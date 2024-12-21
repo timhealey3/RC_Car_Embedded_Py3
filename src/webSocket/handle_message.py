bd6c213f09a895fe0b7f40884b6381c3a1f1c94d
timhealey3 <timhealey34@gmail.com>
2024-12-21 03:53:50 -0500

    Fix messaging

--- src/webSocket/handle_message.py
+++ src/webSocket/handle_message.py
@@ -7,18 +7,35 @@ print("Setting up websocket")
 rc_car = RC_Car()
 
 def incoming_data(data):
-    print(f"Incoming data: {data}")
+    print(f"Debug - Handle Message: Incoming data: {data}")
     if data.get('status') == "START":
-        print("Car is starting")
+        print("Info - Handle Message: Car is starting")
+
+    if data.get('status') == "STOP":
+        print("Info - Handle Message: Car is stopping")
+        rc_car.stop()
+
+    if data.get('status') == "MANUAL":
+        print("Info - Handle Message: Car is in manual mode")
+
+    if data.get('status') == "TRAINING":
+        print("Info - Handle Message: Car is in training mode")
+
+    if data.get('status') == "AUTO":
+            print("Info - Handle Message: Car is in auto mode")
 
     elif data.get('status') == 'NONE':
         if data.get('forward') == "FORWARD":
+            print("Debug - Handle Message: Forward")
             rc_car.accelerate()
         if data.get('forward') == "BACKWARD":
+            print("Debug - Handle Message: Stop")
             rc_car.stop()
         if data.get('turn') == "LEFT":
-            rc_car.turnLeft()
+            print("Debug - Handle Message: Left")
+            rc_car.turn_left()
         if data.get('turn') == "RIGHT":
-            rc_car.turnRight()
+            print("Debug - Handle Message: Right")
+            rc_car.turn_right()
     else:
         print("Unknown command status")
