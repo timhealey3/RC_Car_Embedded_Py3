b3cfc1d4e9c60a58fc01c0125336394213f22707
timhealey3 <timhealey34@gmail.com>
2025-01-26 23:47:45 -0500

    start C rewrite

---
+++ src/car/Control.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include "Control.h"
+#include <pigpio.h>
+
+void GPIOSetup() {
+    // set up outputs
+    gpioSetMode(PIN1_MOTOR1, PI_OUTPUT);
+    gpioSetMode(PIN2_MOTOR1, PI_OUTPUT);
+    gpioSetMode(PIN1_MOTOR2, PI_OUTPUT);
+    gpioSetMode(PIN2_MOTOR2, PI_OUTPUT);
+    gpioSetMode(PIN1_MOTOR3, PI_OUTPUT);
+    gpioSetMode(PIN2_MOTOR3, PI_OUTPUT);
+    gpioSetMode(PIN1_MOTOR4, PI_OUTPUT);
+    gpioSetMode(PIN2_MOTOR4, PI_OUTPUT);
+    // set status
+    gpioWrite(PIN1_MOTOR1, PI_LOW);
+    gpioWrite(PIN1_MOTOR1, PI_LOW);
+    gpioWrite(PIN1_MOTOR2, PI_LOW);
+    gpioWrite(PIN1_MOTOR2, PI_LOW);
+    gpioWrite(PIN1_MOTOR3, PI_LOW);
+    gpioWrite(PIN1_MOTOR3, PI_LOW);
+    gpioWrite(PIN1_MOTOR4, PI_LOW);
+    gpioWrite(PIN1_MOTOR4, PI_LOW);
+}
+
+void pwmDutyCycle(int cycleNum) {
+    gpioPWM(PIN_1A, cycleNum);
+    gpioPWM(PIN_1B, cycleNum);
+    gpioPWM(PIN_2A, cycleNum);
+    gpioPWM(PIN_2B, cycleNum);
+}
+
+void cleanup() {
+    // stop motors
+    // clean gpio
+    // turn off pigpio
+    gpioTerminate();
+}
+
+void pwmFrequency(int freqNum) {
+    gpioSetPWMfrequency(PIN_1A, freqNum);
+    gpioSetPWMfrequency(PIN_1B, freqNum);
+    gpioSetPWMfrequency(PIN_2A, freqNum);
+    gpioSetPWMfrequency(PIN_2B, freqNum);
+}
+
+void PWMSetup() {
+    gpioSetMode(PIN_1A, PI_OUTPUT);
+    gpioSetMode(PIN_1B, PI_OUTPUT);
+    gpioSetMode(PIN_2A, PI_OUTPUT);
+    gpioSetMode(PIN_2B, PI_OUTPUT);
+}
+
+int convertThrottle(int throttle) {
+    int convertedThrottle;
+    if (throttle == 0) {
+    	convertedThrottle = 0;
+    } else if (throttle == 1) {
+    	convertedThrottle = 128;
+    } else if (throttle == 2) {
+	convertedThrottle = 192;
+    } else if (throttle == 3) {
+	convertedThrottle = 224;	    
+    } else {
+	convertedThrottle = 255;
+    }
+    return convertedThrottle;
+}
+
+void motorsForward(int throttle) {
+    int convertedThrottle = convertThrottle(throttle);
+    pwmDutyCycle(convertedThrottle);
+    gpioWrite(PIN1_MOTOR1, PI_HIGH);
+    gpioWrite(PIN2_MOTOR1, PI_LOW);
+    gpioWrite(PIN1_MOTOR2, PI_HIGH);
+    gpioWrite(PIN2_MOTOR2, PI_LOW);
+    gpioWrite(PIN1_MOTOR3, PI_HIGH);
+    gpioWrite(PIN2_MOTOR3, PI_LOW);
+    gpioWrite(PIN1_MOTOR4, PI_HIGH);
+    gpioWrite(PIN2_MOTOR4, PI_LOW);
+}
+
+void motorsStop() {
+    pwmDutyCycle(0);
+    gpioWrite(PIN1_MOTOR1, PI_LOW);
+    gpioWrite(PIN2_MOTOR1, PI_LOW);
+    gpioWrite(PIN1_MOTOR2, PI_LOW);
+    gpioWrite(PIN2_MOTOR2, PI_LOW);
+    gpioWrite(PIN1_MOTOR3, PI_LOW);
+    gpioWrite(PIN2_MOTOR3, PI_LOW);
+    gpioWrite(PIN1_MOTOR4, PI_LOW);
+    gpioWrite(PIN2_MOTOR4, PI_LOW);
+}
+
+int setup() {
+    printf("Set up Car process start\n");
+    // init pigpio
+    if (gpioInitialise() < 0) {
+        printf("pigpio initialization failed.\n");
+        return 1;
+    }
+
+    GPIOSetup();
+    PWMSetup();
+    pwmFrequency(100);
+    pwmDutyCycle(0);
+    return 0;
+}
---
+++ src/car/Control.h
@@ -0,0 +1,20 @@
+#ifndef PINS_H
+#define PINS_H
+
+// PWM pins
+#define PIN_1A    12
+#define PIN_1B    16
+#define PIN_2A    22
+#define PIN_2B    25
+// front motor pins
+#define PIN1_MOTOR1    17
+#define PIN2_MOTOR1    27
+#define PIN1_MOTOR2    23
+#define PIN2_MOTOR2    24
+// back motor pins
+#define PIN1_MOTOR3    26
+#define PIN2_MOTOR3    13
+#define PIN1_MOTOR4    6
+#define PIN2_MOTOR4    5
+
+#endif // PINS_H
